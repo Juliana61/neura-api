b4591b4bd9a08dae30bc590560e9bb43b8f9b42c
Juliana <julianacg82cdn@gmail.com>
2026-04-19 17:40:00 -0500

    database

--- app/database.py
+++ app/database.py
@@ -1,12 +1,13 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 import os
 
-DATABASE_URL = "postgresql://postgres:Wolden6414*@localhost:5432/neura_db"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
